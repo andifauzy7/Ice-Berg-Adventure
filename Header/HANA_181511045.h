c1ca194f86fba1526c011ba1728a567d09355d68
hanakirana25 <hanakiran36@gmail.com>
2019-05-16 11:05:12 +0700

    nambahin desain level, nama modul: readdesain4, readdesain5

--- Header/HANA_181511045.h
+++ Header/HANA_181511045.h
@@ -1,7 +1,12 @@
-//Hana Kirana (181511045)
+// File      : HANA_181511045.h
+// Author    : Hana Kirana (181511045)
+// Deskripsi :
+// D3 JTK'18
+
 #ifndef Hana181511045_H
 #define Hana181511045_H
-#include "ANDI_181511007.h"
+#include "181511007.h"
+
 
 void readdesain1(game arr[BRS][KLM]) //Modul untuk desain level
 {
@@ -48,9 +53,11 @@ arr[9][19].tangga=2;  arr[9][20].tangga=2;  arr[9][21].tangga=2;  arr[9][22].tan
 arr[10][14].tangga=2; arr[10][26].tangga=2; arr[10][27].tangga=2; arr[10][28].tangga=2; arr[10][29].tangga=2; arr[10][30].tangga=2; arr[10][31].tangga=2;
 arr[15][4].tangga=2;  arr[15][5].tangga=2;  arr[15][6].tangga=2;  arr[15][7].tangga=2;  arr[15][8].tangga=2;  arr[16][13].tangga=2; arr[16][14].tangga=2;
 
-// emerald
+// ruby
 arr[21][14].tangga=3; arr[13][19].tangga=3; arr[6][17].tangga=3; arr[7][6].tangga=3; arr[15][29].tangga=3;
 }
+
+
 void readdesain2(game arr[BRS][KLM]) //Modul untuk desain level
 {
 	// bata
@@ -96,11 +103,147 @@ arr[10][12].tangga=2; arr[10][13].tangga=2; arr[10][14].tangga=2; arr[10][15].ta
 arr[10][17].tangga=2; arr[10][18].tangga=2; arr[10][19].tangga=2; arr[10][20].tangga=2; arr[10][21].tangga=2;
 arr[10][22].tangga=2; arr[10][23].tangga=2; arr[10][24].tangga=2; arr[13][16].tangga=2; arr[13][17].tangga=2;
 
-// diamond
+// ruby
 arr[3][17].tangga=3;  arr[7][5].tangga=3;  arr[7][14].tangga=3; arr[7][29].tangga=3;
 arr[13][28].tangga=3; arr[18][6].tangga=3; arr[18][9].tangga=3; arr[18][22].tangga=3;
 
 }
+
+
+void readdesain4 (game arr[BRS][KLM])
+{
+	// bata
+arr[2][13].peta=1; arr[2][14].peta=1; arr[2][15].peta=1; arr[2][16].peta=1; arr[2][17].peta=1;
+arr[2][18].peta=1; arr[2][19].peta=1; arr[2][20].peta=1; arr[2][21].peta=1; arr[2][22].peta=1;
+arr[2][23].peta=1; arr[2][24].peta=1; arr[2][25].peta=1; arr[3][1].peta=1; arr[3][2].peta=1;
+arr[3][3].peta=1; arr[3][4].peta=1; arr[3][5].peta=1; arr[3][6].peta=1; arr[5][8].peta=1;
+arr[5][9].peta=1; arr[5][10].peta=1; arr[5][11].peta=1; arr[5][12].peta=1; arr[5][13].peta=1;
+arr[5][14].peta=1; arr[5][15].peta=1; arr[5][16].peta=1; arr[5][18].peta=1; arr[5][19].peta=1;
+arr[5][20].peta=1; arr[5][21].peta=1; arr[5][22].peta=1; arr[5][23].peta=1; arr[5][25].peta=1;
+arr[5][26].peta=1; arr[7][1].peta=1; arr[7][2].peta=1; arr[7][3].peta=1; arr[7][4].peta=1;
+arr[7][5].peta=1; arr[7][7].peta=1; arr[8][31].peta=1; arr[9][15].peta=1; arr[9][16].peta=1;
+arr[9][17].peta=1; arr[9][18].peta=1; arr[9][19].peta=1; arr[9][20].peta=1; arr[9][21].peta=1;
+arr[9][23].peta=1; arr[9][24].peta=1; arr[10][31].peta=1; arr[10][32].peta=1; arr[10][33].peta=1;
+arr[12][18].peta=1; arr[12][19].peta=1; arr[12][20].peta=1; arr[12][21].peta=1; arr[12][22].peta=1;
+arr[12][23].peta=1; arr[12][24].peta=1; arr[12][25].peta=1; arr[12][26].peta=1; arr[13][18].peta=1;
+arr[13][19].peta=1; arr[13][20].peta=1; arr[13][21].peta=1; arr[13][22].peta=1; arr[13][23].peta=1;
+arr[13][24].peta=1; arr[13][25].peta=1; arr[13][26].peta=1; arr[14][18].peta=1; arr[14][19].peta=1;
+arr[14][20].peta=1; arr[14][21].peta=1; arr[14][22].peta=1; arr[14][23].peta=1; arr[14][24].peta=1;
+arr[14][25].peta=1; arr[14][26].peta=1; arr[15][1].peta=1; arr[15][2].peta=1; arr[15][3].peta=1;
+arr[15][4].peta=1; arr[15][6].peta=1; arr[15][7].peta=1; arr[15][8].peta=1; arr[15][9].peta=1;
+arr[15][10].peta=1; arr[15][11].peta=1; arr[15][12].peta=1; arr[15][13].peta=1; arr[15][14].peta=1;
+arr[15][15].peta=1; arr[15][16].peta=1; arr[15][17].peta=1; arr[15][18].peta=1; arr[15][26].peta=1;
+arr[15][27].peta=1; arr[15][28].peta=1; arr[15][29].peta=1; arr[15][30].peta=1; arr[15][32].peta=1;
+arr[15][33].peta=1; arr[16][1].peta=1; arr[16][2].peta=1; arr[16][3].peta=1; arr[16][4].peta=1;
+arr[16][6].peta=1; arr[16][7].peta=1; arr[16][8].peta=1; arr[16][9].peta=1; arr[16][10].peta=1;
+arr[16][11].peta=1; arr[16][12].peta=1; arr[16][13].peta=1; arr[16][14].peta=1; arr[16][15].peta=1;
+arr[16][16].peta=1; arr[16][17].peta=1; arr[16][18].peta=1; arr[16][26].peta=1; arr[16][27].peta=1;
+arr[16][28].peta=1; arr[16][29].peta=1; arr[16][30].peta=1; arr[16][32].peta=1; arr[16][33].peta=1;
+arr[17][1].peta=1; arr[17][2].peta=1; arr[17][3].peta=1; arr[17][4].peta=1; arr[17][6].peta=1;
+arr[17][7].peta=1; arr[17][8].peta=1; arr[17][9].peta=1; arr[17][10].peta=1; arr[17][11].peta=1;
+arr[17][12].peta=1; arr[17][13].peta=1; arr[17][14].peta=1; arr[17][15].peta=1; arr[17][16].peta=1;
+arr[17][17].peta=1; arr[17][18].peta=1; arr[17][26].peta=1; arr[17][27].peta=1; arr[17][28].peta=1;
+arr[17][29].peta=1; arr[17][30].peta=1; arr[17][32].peta=1; arr[17][33].peta=1; arr[18][1].peta=1;
+arr[18][2].peta=1; arr[18][3].peta=1; arr[18][4].peta=1; arr[18][6].peta=1; arr[18][7].peta=1;
+arr[18][8].peta=1; arr[18][9].peta=1; arr[18][10].peta=1; arr[18][11].peta=1; arr[18][12].peta=1;
+arr[18][13].peta=1; arr[18][14].peta=1; arr[18][15].peta=1; arr[18][16].peta=1; arr[18][17].peta=1;
+arr[18][18].peta=1; arr[18][21].peta=1; arr[18][22].peta=1; arr[18][23].peta=1; arr[18][26].peta=1;
+arr[18][27].peta=1; arr[18][28].peta=1; arr[18][29].peta=1; arr[18][30].peta=1; arr[18][32].peta=1;
+arr[18][33].peta=1; arr[19][21].peta=1; arr[19][22].peta=1; arr[19][23].peta=1; arr[20][21].peta=1;
+arr[20][22].peta=1; arr[20][23].peta=1;
+
+// tangga
+arr[2][12].objek=1; arr[3][12].objek=1; arr[4][12].objek=1; arr[2][26].objek=1; arr[3][26].objek=1;
+arr[4][26].objek=1; arr[3][7].objek=1; arr[4][7].objek=1; arr[5][7].objek=1; arr[6][7].objek=1;
+arr[7][6].objek=1; arr[8][6].objek=1; arr[9][6].objek=1; arr[10][6].objek=1; arr[11][6].objek=1;
+arr[12][6].objek=1; arr[13][6].objek=1; arr[14][6].objek=1; arr[5][17].objek=1; arr[6][17].objek=1;
+arr[6][17].objek=1; arr[6][17].objek=1; arr[5][24].objek=1; arr[6][24].objek=1; arr[6][24].objek=1;
+arr[6][24].objek=1; arr[9][14].objek=1; arr[10][14].objek=1; arr[11][14].objek=1; arr[12][14].objek=1;
+arr[9][22].objek=1; arr[10][22].objek=1; arr[11][22].objek=1; arr[12][17].objek=1; arr[13][17].objek=1;
+arr[14][17].objek=1; arr[12][27].objek=1; arr[13][27].objek=1; arr[14][27].objek=1; arr[15][5].objek=1;
+arr[16][5].objek=1; arr[17][5].objek=1; arr[18][5].objek=1; arr[19][5].objek=1; arr[20][5].objek=1;
+arr[21][5].objek=1; arr[15][31].objek=1; arr[16][31].objek=1; arr[17][31].objek=1; arr[18][31].objek=1;
+arr[19][31].objek=1; arr[20][31].objek=1; arr[18][20].objek=1; arr[19][20].objek=1; arr[20][20].objek=1;
+arr[21][20].objek=1; arr[18][24].objek=1; arr[19][24].objek=1; arr[20][24].objek=1; arr[21][24].objek=1;
+
+// tali
+arr[6][27].objek=2; arr[6][28].objek=2; arr[7][29].objek=2; arr[7][30].objek=2; arr[11][7].objek=2;
+arr[11][8].objek=2; arr[11][9].objek=2; arr[11][10].objek=2; arr[11][11].objek=2; arr[11][12].objek=2;
+arr[11][12].objek=2;
+
+// ruby
+arr[1][18].objek=3; arr[1][20].objek=3; arr[2][3].objek=3; arr[4][21].objek=3; arr[6][4].objek=3;
+arr[9][31].objek=3; arr[14][12].objek=3; arr[21][2].objek=3; arr[21][28].objek=3;
+}
+
+
+void readdesain5 (game arr[BRS][KLM])
+{
+	//bata
+arr[3][2].peta=1; arr[3][3].peta=1; arr[3][4].peta=1; arr[3][5].peta=1; arr[3][6].peta=1;
+arr[3][7].peta=1; arr[3][8].peta=1; arr[3][9].peta=1; arr[3][10].peta=1; arr[3][11].peta=1;
+arr[3][12].peta=1; arr[3][13].peta=1; arr[3][14].peta=1; arr[3][15].peta=1; arr[3][16].peta=1;
+arr[3][17].peta=1; arr[3][18].peta=1; arr[3][19].peta=1; arr[3][20].peta=1; arr[5][1].peta=1;
+arr[5][2].peta=1; arr[5][3].peta=1; arr[5][4].peta=1; arr[5][5].peta=1; arr[5][6].peta=1;
+arr[5][7].peta=1; arr[5][8].peta=1; arr[5][9].peta=1; arr[5][10].peta=1; arr[5][11].peta=1;
+arr[5][12].peta=1; arr[5][13].peta=1; arr[5][15].peta=1; arr[5][16].peta=1; arr[5][17].peta=1;
+arr[5][18].peta=1; arr[5][19].peta=1; arr[5][20].peta=1; arr[5][21].peta=1; arr[5][30].peta=1;
+arr[5][31].peta=1; arr[5][31].peta=1; arr[5][33].peta=1; arr[6][1].peta=1; arr[6][2].peta=1;
+arr[6][3].peta=1; arr[6][4].peta=1; arr[6][5].peta=1; arr[6][6].peta=1; arr[6][7].peta=1;
+arr[6][8].peta=1; arr[6][9].peta=1; arr[6][10].peta=1; arr[6][11].peta=1; arr[6][12].peta=1;
+arr[7][1].peta=1; arr[7][2].peta=1; arr[7][3].peta=1; arr[7][4].peta=1; arr[7][5].peta=1;
+arr[7][6].peta=1; arr[7][7].peta=1; arr[7][8].peta=1; arr[7][9].peta=1; arr[7][10].peta=1;
+arr[7][11].peta=1; arr[8][1].peta=1; arr[8][2].peta=1; arr[8][3].peta=1; arr[8][4].peta=1;
+arr[8][5].peta=1; arr[8][6].peta=1; arr[8][7].peta=1; arr[8][8].peta=1; arr[8][9].peta=1;
+arr[8][10].peta=1; arr[9][1].peta=1; arr[9][2].peta=1; arr[9][3].peta=1; arr[9][7].peta=1;
+arr[9][8].peta=1; arr[9][9].peta=1; arr[10][1].peta=1; arr[10][2].peta=1; arr[10][3].peta=1;
+arr[10][4].peta=1; arr[10][5].peta=1; arr[10][6].peta=1; arr[10][7].peta=1; arr[10][8].peta=1;
+arr[10][17].peta=1; arr[10][18].peta=1; arr[10][19].peta=1; arr[10][20].peta=1; arr[10][21].peta=1;
+arr[10][22].peta=1; arr[10][23].peta=1; arr[10][24].peta=1; arr[10][25].peta=1; arr[10][26].peta=1;
+arr[10][27].peta=1; arr[10][28].peta=1; arr[10][30].peta=1; arr[10][31].peta=1; arr[10][32].peta=1;
+arr[10][33].peta=1; arr[14][2].peta=1; arr[14][3].peta=1; arr[14][4].peta=1; arr[14][5].peta=1;
+arr[14][6].peta=1; arr[14][7].peta=1; arr[15][2].peta=1; arr[15][3].peta=1; arr[15][4].peta=1;
+arr[15][5].peta=1; arr[15][6].peta=1; arr[15][7].peta=1; arr[15][8].peta=1; arr[15][9].peta=1;
+arr[15][10].peta=1; arr[15][11].peta=1; arr[15][12].peta=1; arr[15][13].peta=1; arr[15][14].peta=1;
+arr[15][15].peta=1; arr[15][30].peta=1; arr[15][31].peta=1; arr[15][32].peta=1; arr[15][33].peta=1;
+arr[17][2].peta=1; arr[17][3].peta=1; arr[17][4].peta=1; arr[17][5].peta=1; arr[17][6].peta=1;
+arr[17][7].peta=1; arr[17][8].peta=1; arr[17][18].peta=1; arr[17][19].peta=1; arr[17][20].peta=1;
+arr[17][21].peta=1; arr[17][22].peta=1; arr[17][23].peta=1; arr[17][24].peta=1; arr[17][25].peta=1;
+arr[17][26].peta=1; arr[17][27].peta=1; arr[21][1].peta=1; arr[21][2].peta=1; arr[21][3].peta=1;
+arr[21][4].peta=1; arr[21][5].peta=1; arr[21][6].peta=1; arr[21][7].peta=1; arr[21][8].peta=1;
+arr[21][9].peta=1; arr[21][10].peta=1; arr[21][11].peta=1; arr[21][12].peta=1; arr[21][13].peta=1;
+arr[21][14].peta=1; arr[21][15].peta=1; arr[21][16].peta=1; arr[21][17].peta=1; arr[21][18].peta=1;
+arr[21][19].peta=1; arr[21][20].peta=1; arr[21][21].peta=1; arr[21][22].peta=1; arr[21][23].peta=1;
+arr[21][24].peta=1; arr[21][25].peta=1; arr[21][26].peta=1; arr[21][27].peta=1; arr[21][28].peta=1;
+arr[21][29].peta=1; arr[21][30].peta=1; arr[21][31].peta=1; arr[21][32].peta=1; arr[21][33].peta=1;
+
+//tangga
+arr[3][1].objek=1; arr[4][1].objek=1; arr[3][21].objek=1; arr[4][21].objek=1; arr[5][14].objek=1;
+arr[6][14].objek=1; arr[6][13].objek=1; arr[7][13].objek=1; arr[7][12].objek=1; arr[8][12].objek=1;
+arr[8][11].objek=1; arr[9][11].objek=1;  arr[9][10].objek=1; arr[10][10].objek=1; arr[10][9].objek=1;
+arr[11][9].objek=1; arr[12][9].objek=1; arr[13][9].objek=1; arr[14][9].objek=1; arr[7][20].objek=1;
+arr[8][20].objek=1; arr[9][20].objek=1; arr[14][1].objek=1; arr[15][1].objek=1; arr[16][1].objek=1;
+arr[17][1].objek=1; arr[18][1].objek=1; arr[19][1].objek=1; arr[20][1].objek=1; arr[10][16].objek=1;
+arr[11][16].objek=1; arr[12][16].objek=1; arr[13][16].objek=1; arr[14][16].objek=1; arr[15][16].objek=1;
+arr[16][16].objek=1; arr[17][16].objek=1; arr[18][16].objek=1; arr[19][16].objek=1; arr[20][16].objek=1;
+arr[2][29].objek=1; arr[3][29].objek=1; arr[4][29].objek=1; arr[5][29].objek=1; arr[6][29].objek=1;
+arr[7][29].objek=1; arr[8][29].objek=1; arr[9][29].objek=1; arr[10][29].objek=1; arr[11][29].objek=1;
+arr[12][29].objek=1; arr[13][29].objek=1; arr[14][29].objek=1; arr[15][29].objek=1; arr[16][29].objek=1;
+arr[17][29].objek=1; arr[18][29].objek=1; arr[19][29].objek=1; arr[20][29].objek=1;
+
+//tali
+arr[4][22].objek=2; arr[4][23].objek=2; arr[4][24].objek=2; arr[4][25].objek=2; arr[4][26].objek=2;
+arr[4][27].objek=2; arr[4][28].objek=2; arr[7][14].objek=2; arr[7][15].objek=2; arr[7][16].objek=2;
+arr[7][17].objek=2; arr[7][18].objek=2; arr[7][19].objek=2; arr[14][17].objek=2; arr[14][18].objek=2;
+arr[14][19].objek=2; arr[14][20].objek=2; arr[14][21].objek=2; arr[14][22].objek=2; arr[14][23].objek=2;
+arr[14][24].objek=2; arr[14][25].objek=2; arr[14][26].objek=2; arr[14][27].objek=2; arr[14][28].objek=2;
+
+//ruby
+arr[2][8].objek=3; arr[2][16].objek=3; arr[4][32].objek=3; arr[9][3].objek=3; arr[9][6].objek=3;
+arr[9][24].objek=3; arr[9][32].objek=3; arr[14][12].objek=3; arr[16][19].objek=3; arr[16][26].objek=3;
+}
+
+
 void buatpeta(game arr[BRS][KLM], int* BRS_, int* KLM_,int lvl) //untuk membuat array
 {
     for (int i=0 ; i<BRS ; i++){
@@ -129,6 +272,8 @@ void buatpeta(game arr[BRS][KLM], int* BRS_, int* KLM_,int lvl) //untuk membuat 
     *KLM_ = KLM/2;
     inspemain(arr,*BRS_, *KLM_);
 }
+
+
 void tampilall(game arr[BRS][KLM])
 {
     for (int i = 0; i < BRS; i++){
@@ -155,6 +300,8 @@ void tampilall(game arr[BRS][KLM])
     }
 
 }
+
+
 void bawah(game arr[BRS][KLM], int BRS_, int KLM_,int a)
 {
     for (int i = 0; i <=2; i++){
